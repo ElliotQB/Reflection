5e36c14aabfe6af4fb1a20e6ce2c95986737dc8c
ElliotQB <elliotbusch40@gmail.com>
2024-10-24 13:16:35 -0500

    Added RectangleCollision()
    And player collision

--- game.go
+++ game.go
@@ -1,7 +1,13 @@
 package main
 
+import rl "github.com/gen2brain/raylib-go/raylib"
+
 type Game struct {
-	blocks Block
-	player Player
-	input  Input
+	Blocks []Block
+	Player []Player
+	Input  []Input
+}
+
+func RectangleCollision(pos1 rl.Vector2, size1 rl.Vector2, pos2 rl.Vector2, size2 rl.Vector2) bool {
+	return pos1.X > pos2.X && pos1.X+size1.X < pos2.X+size2.X && pos1.Y > pos2.Y && pos1.Y+size1.Y < pos2.Y+size2.Y
 }
--- player.go
+++ player.go
@@ -6,7 +6,7 @@ type Player struct {
 	X    float32
 	Y    float32
 	Size float32
-	game *Game
+	Game *Game
 }
 
 func NewPlayer(x float32, y float32, gameState *Game) Player {
@@ -17,8 +17,15 @@ func (p *Player) PlayerTick() {
 
 }
 
-func (p *Player) PlayerCollision() {
+func (p *Player) PlayerCollision() bool {
+	for i := 0; i < len(p.Game.Blocks); i++ {
+		block := p.Game.Blocks[i]
 
+		if RectangleCollision(rl.NewVector2(p.X, p.Y), rl.NewVector2(p.Size, p.Size), rl.NewVector2(block.X, block.Y), rl.NewVector2(block.Width, block.Height)) {
+			return true
+		}
+	}
+	return false
 }
 
 func (p *Player) DrawPlayer() {
