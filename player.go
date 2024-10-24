b59dd9b7cca351ebe4a7b1679365e8429cb4f0a5
ElliotQB <elliotbusch40@gmail.com>
2024-10-24 12:41:31 -0500

    Created basic functionality for player and game
    Player can draw and game can game

--- player.go
+++ player.go
@@ -1,14 +1,26 @@
 package main
 
+import rl "github.com/gen2brain/raylib-go/raylib"
+
 type Player struct {
-	X float32
-	Y float32
+	X    float32
+	Y    float32
+	Size float32
+	game *Game
 }
 
-func NewPlayer() {
+func NewPlayer(x float32, y float32, gameState *Game) Player {
+	return Player{x, y, 10, gameState}
+}
+
+func (p *Player) PlayerTick() {
 
 }
 
-// func (*p Player) PlayerTick()
+func (p *Player) PlayerCollision() {
 
-// func (*p Player) PlayerCollision()
+}
+
+func (p *Player) DrawPlayer() {
+	rl.DrawRectangle(int32(p.X-(p.Size/2)), int32(p.Y-(p.Size/2)), int32(p.Size), int32(p.Size), rl.DarkBlue)
+}
