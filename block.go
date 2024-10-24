9339b0e07b46862157ae8363f96c897e11f4cb47
Sam <sammipoole310@icloud.com>
2024-10-24 12:35:07 -0500

    created block.go

--- block.go
+++ block.go
@@ -1,5 +1,7 @@
 package main
 
+import rl "github.com/gen2brain/raylib-go/raylib"
+
 type Block struct {
 	X      float32
 	Y      float32
@@ -7,6 +9,10 @@ type Block struct {
 	Height float32
 }
 
-func NewBlock() {
+func NewBlock(x float32, y float32, width float32, height float32) Block {
+	return Block{x, y, width, height}
+}
 
+func (b *Block) DrawBlock() {
+	rl.DrawRectangle(int32(b.X), int32(b.Y), int32(b.Width), int32(b.Height), rl.Pink)
 }
